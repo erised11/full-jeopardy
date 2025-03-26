c77d1de32b5e81a60deb57332eeb74dec6c6e57e
Brandon Ryan <brandon.ryan_ext@clariant.com>
2025-03-26 22:18:19 +0100

    Updated final jeopardy question

--- src/components/FinalJeopardyModal.tsx
+++ src/components/FinalJeopardyModal.tsx
@@ -2,7 +2,7 @@ export const FinalJeopardyModal = () => {
   return (
     <div className="absolute inset-0 left-0 top-0 w-[100vw] bg-jeopardy h-[100vh] flex items-center justify-center text-white cursor-pointer">
       <p className="rounded-lg text-center text-[100px] w-4/5 font-korinna textShadow uppercase">
-        What is the total amount of coffee consumed every year?
+        What book holds the record for the fastest selling book in history?
       </p>
     </div>
   );
